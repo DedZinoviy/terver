20dbd035f0f17ec164cd0420b7d7c6c61b466167
dima-bolgov.02@mail.ru <dima-bolgov.02@mail.ru>
2022-02-20 09:49:01 +0300

    release

--- combinatoric.py
+++ combinatoric.py
@@ -4,21 +4,26 @@ class Combinatoric:
     def __init__(self):
         pass
 
+    
+    '''Вычислить число сочетаний без повторений'''
+    def combinations_without_repeats(n, m):
+        arr = [m, n-m]     # определить основания факториалов в знаменателе
+        maximum = max(arr) # определить максимум из оснований факториалов знаменателя
+        minimum = min(arr) # определить минимум из оснований факториалов знаменателя
 
-    def combinations_without_repeats(self, n, m):
-        arr = [m, n-m]
-        maximum = max(arr)
-        minimum = min(arr)
+        # Определить число сочетаний. В числителе n!, сокращенный на факториал максимума, в знаменателе факториал минимума 
         result = np.prod(np.arange(maximum + 1, n + 1, 1, dtype=np.object)) // np.math.factorial(minimum)     
         return result
 
 
-    def combinations_with_repeats(self, n, m):
-        n = n + m - 1
-        result = self.combinations_without_repeats(n, m)
+    '''Вычислить число сочетаний с повторениями'''
+    def combinations_with_repeats(n, m):
+        n = n + m - 1                                            # переопределить n для перехода к формуле сочетаний
+        result = Combinatoric.combinations_without_repeats(n, m) # определить число сочетаний для нового значения n
         return result
 
 
-    def accommodation_with_repeats(self, n, m):
+    '''Вычислить число размещений с повторениями'''
+    def accommodation_with_repeats(n, m):
         return n**m
 
--- main.py
+++ main.py
@@ -5,36 +5,49 @@ import sys
 from combinatoric import Combinatoric
 
 class mywindow(QtWidgets.QMainWindow):
+
+    '''Конструктор главного окна'''
     def __init__(self):
         super(mywindow, self).__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+
+        # Установить формулу по-умолчанию
         pixmap = QPixmap("img/sochetaniya.png")
         self.ui.formula_img.setPixmap(pixmap)
-        self.combinatoric = Combinatoric()
+
+        # Установить коннекты
         self.ui.solve_button.clicked.connect(self.solve)
         self.ui.comboBox.currentIndexChanged.connect(self.setImg)
     
+    '''Изменить изображение формулы'''
     def setImg(self):
         img = ["sochetaniya.png", "sochetaniya_with_repeat.png", "accomodattion_with_repeat.png"]
         selection = self.ui.comboBox.currentIndex()
         pixmap = QPixmap("img/" + img[selection])
         self.ui.formula_img.setPixmap(pixmap)
 
+    '''Произвести расчет выбранной формулы'''
     def solve(self):
+        # Считать введенные данные
         selection = self.ui.comboBox.currentIndex()
         n = self.ui.n_spinBox.value()
         m = self.ui.m_spinBox.value()
+
+        # В зависимости от выбранной формулы произвести вычисления
         if selection == 0 and n >= m:
-            result = self.combinatoric.combinations_without_repeats(n, m)
+            result = Combinatoric.combinations_without_repeats(n, m)
         elif selection == 1:
-            result = self.combinatoric.combinations_with_repeats(n, m)
+            result = Combinatoric.combinations_with_repeats(n, m)
         elif selection == 2:
-            result = self.combinatoric.accommodation_with_repeats(n, m)
+            result = Combinatoric.accommodation_with_repeats(n, m)
         else:
             result = "m должно быть меньше n"
+
+        # Вывести полученное значение в поле ответа
         self.ui.result_textEdit.setText(str(result))
- 
+
+# Запуск главного окна 
 if __name__ == '__main__': 
     app = QtWidgets.QApplication([])
     application = mywindow()
